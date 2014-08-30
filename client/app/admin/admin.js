6ae3cfe6183e72e6ad031b57479a3473ac2e0a4b
Guilherme Andrade <guilherme.andrade19@hotmail.com>
2014-08-30 02:33:48 -0300

    start app

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('jobApplicationApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
